e8cbeaa9dec7c41fc12eba4f76379c7a8f6b7813
AndyLPK247 <AndyLPK247@gmail.com>
2018-03-03 21:24:54 -0500

    test_api_product test file structure and notes

---
+++ tests/test_api_product.py
@@ -0,0 +1,30 @@
+"""
+This module contains Web API tests for the following API:
+http://pltestautomationsample.azurewebsites.net/api/product/
+
+The source code for this API is located at:
+https://github.com/LouisSheridan/SampleWebAPI
+
+Based on the source code, it looks like there are two endpoints:
+One for all products, and one for getting an individual product by ID.
+It looks like only GET calls are available.
+
+The tests are written using the pytest framework.
+For simplicity, the tests are functions and not classes.
+"""
+
+# --------------------------------------------------
+# Imports
+# --------------------------------------------------
+
+import pytest
+
+
+# --------------------------------------------------
+# Tests for /api/product/
+# --------------------------------------------------
+
+# --------------------------------------------------
+# Tests for /api/product/<id>/
+# --------------------------------------------------
+
